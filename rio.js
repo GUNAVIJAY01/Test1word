107d16aaac2c4c5b3872022a5b2ca22dd4ccca60
GUNAVIJAY01 <gunanathan0@gmail.com>
2023-09-28 21:24:04 +0530

    update

---
+++ rio.js
@@ -0,0 +1,35 @@
+// 1. How many types of export are there? 
+    // ->there are two types
+    //     ->named export
+    //     ->default export
+// 2. In React.js which one of the following is used to create a class for Inheritance ?
+            // ->extends
+
+// 3. What is the default port number in which the application run ?
+
+        // ->localhost:3000
+// 4. Which symbol is used to update Patch version?
+
+        // -> '~' symbol
+// 5. What is the output of the following code?
+const make = 'Ford';
+const model = 'Mustang';
+const car = { make, model };
+console.log(car);
+    // ->output:{make: 'Ford', model: 'Mustang'}
+// 6. Which of the life cycle method in a React Component is called after the component is rendered for the first time?
+        // ->componentDidMount()    
+
+// 7. Which attribute is used to populate value?
+
+    // ->value attribute
+
+// 8. What should the console read when the following code is run?
+const [, , animal] = ['Horse', 'Mouse', 'Cat'];
+console.log(animal);
+    // ->output:cat
+// 9. Whenever I update the state which lifecycle method is called?
+
+        // ->componentDidUpdate()
+// 10. Use of componentDidMount
+        //   -> we can execute the React code when the component has already been placed in the DOM (Document Object Model).  
